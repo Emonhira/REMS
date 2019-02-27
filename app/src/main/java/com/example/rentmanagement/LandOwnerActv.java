19500e1f0c35657b980d3498ade1d7970d57f08b
Abdullah Al Mehedi Hira <abdullahalmehedihira@gmail.com>
2019-02-27 23:32:24 +0600

    Initial commit

---
+++ app/src/main/java/com/example/rentmanagement/LandOwnerActv.java
@@ -0,0 +1,13 @@
+package com.example.rentmanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LandOwnerActv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_land_owner_actv);
+    }
+}
---
+++ app/src/main/java/com/example/rentmanagement/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.rentmanagement;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button button;
+     public void land(){
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent x = new Intent(MainActivity.this,LandOwnerActv.class);
+
+                startActivity(x);
+
+            }
+        });
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        land();
+    }
+}
