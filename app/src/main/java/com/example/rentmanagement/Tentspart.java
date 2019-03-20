b2ab885a506acf33cfb9e214b8b2dc1e9e5fdd8e
Abdullah Al Mehedi Hira <abdullahalmehedihira@gmail.com>
2019-03-20 22:28:40 +0600

    few changes

---
+++ app/src/main/java/com/example/rentmanagement/Tentspart.java
@@ -0,0 +1,13 @@
+package com.example.rentmanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tentspart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentspart);
+    }
+}
