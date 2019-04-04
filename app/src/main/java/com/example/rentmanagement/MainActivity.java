1c0669a6e82e3f9c20dd536f5293b29d8c917891
Abdullah Al Mehedi Hira <abdullahalmehedihira@gmail.com>
2019-04-05 04:28:20 +0600

    errors removed

--- app/src/main/java/com/example/rentmanagement/MainActivity.java
+++ app/src/main/java/com/example/rentmanagement/MainActivity.java
@@ -39,44 +39,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-    public Button button3;
-    public void owner(){
-        button3=(Button)findViewById(R.id.button3);
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent a = new Intent(MainActivity.this,owner_part.class);
-
-                startActivity(a);
-
-            }
-        });
-
-    }
-    public Button button4;
-    public void tenant(){
-        button4=(Button)findViewById(R.id.button4);
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent z = new Intent(MainActivity.this,tenants_proceed_part.class);
-
-                startActivity(z);
-
-            }
-        });
-
-    }
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         land();
         tent();
-        tenant();
-        owner();
+
+
     }
 }
