038515518a084dfa8339c34fd2bb610ddd20e261
Abdullah Al Mehedi Hira <abdullahalmehedihira@gmail.com>
2019-04-10 00:01:53 +0600

    database part

--- app/src/main/java/com/example/rentmanagement/DatabaseHelper.java
+++ app/src/main/java/com/example/rentmanagement/DatabaseHelper.java
@@ -8,16 +8,20 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper  extends SQLiteOpenHelper {
     public static final String DATABASE_NAME="Register.db";
     public static final String TABLE_NAME="Register";
-    public static final String COL_1= "Name";
-    public static final String COL_2="Number";
+    public static final String COL_1= "ID";
+    public static final String COL_2="Name";
+    public static final String COL_3="Number";
 
     public DatabaseHelper(Context context) {
 
-        super(context, , factory, version);
+        super(context, DATABASE_NAME,null,1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+     sqLiteDatabase.execSQL("CREATE TABLE" +TABLE_NAME+ "(ID INTEGER PRIMARY KEY AUTO INCREMENT, Name TEXT, Number TEXT)");
+
+
 
     }
 
